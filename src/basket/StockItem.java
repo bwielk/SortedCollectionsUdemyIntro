c4b95992a83d0844c1535eec5d6414187d4585e5
bwielk <bwielk@gmail.com>
2018-03-25 13:28:27 +0200

    Initial commit

---
+++ src/basket/StockItem.java
@@ -0,0 +1,12 @@
+package basket;
+
+public class StockItem implements Comparable{
+	
+	private final String name;
+	private double price;
+	private int quantityStock = 0;
+	
+	public StockItem(){
+		
+	}
+}
